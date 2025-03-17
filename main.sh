5ba0eda951a8141952af6536ea1da6815430f1da
Aziz Emir <101357256+AzizEmir@users.noreply.github.com>
2025-03-17 17:50:48 +0000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,10 @@
+_lxc_start_complete() {
+    local containers
+    containers=$(find /var/lib/lxc -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
+    selected=$(echo "$containers" | fzf --preview "cat /var/lib/lxc/{}/index.md; echo \"\n---\n\"; cat /var/lib/lxc/{}/config" --preview-window=up:70%:wrap)
+    if [[ -n "$selected" ]]; then
+        echo "$selected"
+    fi
+}
+
+complete -F _lxc_start_complete lxc-start lxc-stop lxc-attach
