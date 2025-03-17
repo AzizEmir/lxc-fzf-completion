370db637a42331686ad88badb9cb24b28c661986
Aziz Emir <101357256+AzizEmir@users.noreply.github.com>
2025-03-17 22:31:36 +0000

    Update main.sh: find with sort

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 _lxc_start_complete() {
     local containers
-    containers=$(find /var/lib/lxc -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
+    containers=$(find /var/lib/lxc -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | sort)
     selected=$(echo "$containers" | fzf --preview "cat /var/lib/lxc/{}/index.md; echo \"\n---\n\"; cat /var/lib/lxc/{}/config" --preview-window=up:70%:wrap)
     if [[ -n "$selected" ]]; then
         echo "$selected"
